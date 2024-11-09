5aa5c2deb2c35455f754e20944c119dd8ca1ee0d
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 04:36:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,21 @@ export default function HomePage() {
       <Header/>
       <Herosection/>
       <section className="flex justify-between align items-center m-14 p-1">
-        <h1 className="text-2xl font-bold text-center">Women</h1>
+        <h1 className="text-xl font-bold text-center">Women</h1>
         
         <div><Image src="/Image/ladiesperfume5.jpg" alt="ladies perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Elegant</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.4500/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Elegant</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.4500/-</p>
         </div>
 
         <div><Image src="/Image/ladiesperfume1.jpg" alt="ladies perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Burberry</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.6500/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Burberry</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.6500/-</p>
         </div>
 
         <div><Image src="/Image/ladiesperfume3.jpg" alt="ladies perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Bloom</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.3500/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Bloom</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.3500/-</p>
         </div>
       </section>
 
@@ -31,18 +31,18 @@ export default function HomePage() {
         <h1 className="text-2xl font-bold text-center">Men</h1>
         
         <div><Image src="/Image/gentsperfume5.jpg" alt="gents perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Coach</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.5500/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Coach</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.5500/-</p>
         </div>
 
         <div><Image src="/Image/gentsperfume2.jpg" alt="gents perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Prada</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.3500/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Prada</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.3500/-</p>
         </div>
 
         <div><Image src="/Image/gentsperfume3.jpg" alt="gents perfume" width={300} height={300}/>
-        <h1 className="text-xl font-semibold text-center m-2">Boss</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.7000/-</p>
+        <h1 className="text-sm font-semibold text-center m-2">Boss</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.7000/-</p>
         </div>
       </section>
       <Footer/>
