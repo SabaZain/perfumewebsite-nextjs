f6849fc5438614e98aa8795e28a394b2fa2574a7
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 04:56:29 +0500

    Update page.tsx

--- src/app/Newarrivalwomen/page.tsx
+++ src/app/Newarrivalwomen/page.tsx
@@ -7,10 +7,10 @@ const Newarrivalmen = () => {
         <Header/>
         
         <h1 className="text-3xl text-center font-bold font-serif text-blue-950 pt-2 m-5">New Arrival Collection For Women</h1>
-        <main className="flex justify-center align items-center p-1 m-4 gap-6 ">
-        <Image src="/Image/ladiesperfume4.jpg" alt="gents perfume" width={350} height={350}/>
-        <h1 className="text-xl font-semibold text-center m-2">Rosemerry Gold</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.4000/-</p>
+        <main className="flex-col sm:flex flex  justify-center align items-center h-screen ">
+        <Image src="/Image/ladiesperfume4.jpg" alt="gents perfume" width={300} height={300}/>
+        <h1 className="text-sm font-semibold text-center">Rosemerry Gold</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.4000/-</p>
         
         </main>
     </div>
