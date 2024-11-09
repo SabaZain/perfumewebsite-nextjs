9269cf5684b16aa84a1ee5ed7d59a1720b4d4a66
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 04:34:00 +0500

    Update Header.tsx

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,18 +1,15 @@
 import Link from "next/link";
-import Image from "next/image";
-
-
 
 function Header() {
   return (
-    <header className="bg-black text-white text-xl h-25 flex justify-between align items-center border-4 rounded-md m-0">
+    <header className="bg-black text-white text-xl sm:text-base h-auto sm:h-22 flex-row justify-between align items-center border-4 rounded-md m-0">
 
       <nav className="flex gap-10">
-        <Image src="/Image/perfumelogo.jpg" alt="logo" width={80} height={80} />
+        
         <h1 className="text-yellow-400 text-2xl flex justify-center p-6 pl-1" >Smart Perfumes</h1>
 
 
-        <ul className="flex justify-between align items-center gap-6 p-6 pr-10">
+        <ul className="flex-col sm:flex gap-2 sm:gap-6 p-2 sm:p-6  pr-10">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/About">About</Link></li>
           <li><Link href="/Newarrivalwomen">New Arrival Women</Link></li>
@@ -27,3 +24,5 @@ function Header() {
 
 export default Header
 
+
+     
