59191ec4ecf3123fd0e0d967ea9a6244943fc33e
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 13:47:36 +0500

    Update page.tsx

--- src/app/Mostdemanding/page.tsx
+++ src/app/Mostdemanding/page.tsx
@@ -8,7 +8,7 @@ const Mostdemanding = () => {
         
         <h1 className="text-3xl text-center font-bold font-serif text-blue-950 pt-2 m-5">Most Demanding</h1>
         <main className="flex justify-center align items-center p-1 m-4 gap-6 ">
-        <Image src="/Image/gentsperfume1.jpg" alt="gents perfume" width={250} height={250}/>
+        <Image src="/Image/gentsperfume1.jpg" alt="gents perfume" width={200} height={200}/>
         <h1 className="text-sm font-semibold text-center m-2">Valentino</h1>
         <p className="text-sm font-semibold text-center">Price: Rs.4000/-</p>
         </main>
