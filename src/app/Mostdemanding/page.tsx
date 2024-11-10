29ebde5e8300bd66b221825932930a160b597b83
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 05:02:26 +0500

    Update page.tsx

--- src/app/Mostdemanding/page.tsx
+++ src/app/Mostdemanding/page.tsx
@@ -8,15 +8,15 @@ const Mostdemanding = () => {
         
         <h1 className="text-3xl text-center font-bold font-serif text-blue-950 pt-2 m-5">Most Demanding</h1>
         <main className="flex justify-center align items-center p-1 m-4 gap-6 ">
-        <Image src="/Image/gentsperfume1.jpg" alt="gents perfume" width={350} height={350}/>
-        <h1 className="text-xl font-semibold text-center m-2">Valentino</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.4000/-</p>
+        <Image src="/Image/gentsperfume1.jpg" alt="gents perfume" width={250} height={250}/>
+        <h1 className="text-sm font-semibold text-center m-2">Valentino</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.4000/-</p>
         </main>
 
         <main className="flex justify-center align items-center p-1 m-4 gap-6 ">
-        <Image src="/Image/ladiesperfume6.jpg" alt="gents perfume" width={350} height={350}/>
-        <h1 className="text-xl font-semibold text-center m-2">Pinksome</h1>
-        <p className="text-xl font-semibold text-center">Price: Rs.4400/-</p>
+        <Image src="/Image/ladiesperfume6.jpg" alt="gents perfume" width={250} height={250}/>
+        <h1 className="text-sm font-semibold text-center m-2">Pinksome</h1>
+        <p className="text-sm font-semibold text-center">Price: Rs.4400/-</p>
         </main>
     </div>
   )
