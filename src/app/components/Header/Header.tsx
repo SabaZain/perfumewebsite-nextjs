0bb9b8c8c2708488f2fb6166bf4453adef2eb62a
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-10 13:37:15 +0500

    Update Header.tsx

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header className="bg-black text-white text-xl sm:text-base h-auto sm:h-22 flex-row justify-between align items-center border-4 rounded-md m-0">
+    <header className="bg-black text-white flex sm:flex-row items-center justify-between h-auto sm:h-22 border-4 rounded-md m-0 px-3 sm:px-80">
 
       <nav className="flex gap-10">
         
         <h1 className="text-yellow-400 text-2xl flex justify-center p-6 pl-1" >Smart Perfumes</h1>
 
 
-        <ul className="flex-col sm:flex gap-2 sm:gap-6 p-2 sm:p-6  pr-10">
+        <ul className="flex  flex-col sm:flex-row sm:p-15 p-2 gap-2 sm:gap-12">
           <li><Link href="/">Home</Link></li>
           <li><Link href="/About">About</Link></li>
           <li><Link href="/Newarrivalwomen">New Arrival Women</Link></li>
